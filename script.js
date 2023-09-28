679b28585320439042b3c8b429133636d2b5fc2c
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-09-28 16:16:21 -0400

    Add function in module to add contents of array to display

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 
 const GameBoardModule = (() => {
-    let gameBoard = [];
+    let gameBoard = ['X', 'O', 'X', 'O', 'X', 'O'];
+
+    const displayArray = () => {
+        const displayVar = document.querySelector('.game-container');
+            gameBoard.forEach((element) => {
+                displayVar.innerHTML += element;
+            })
+        // const displayVar = document.querySelector('.game-container');
+        // for (let i = 0; i < gameBoard.length; i++) {
+        //     displayVar.innerHTML += gameBoard[i];
+        // }
+    }
+
+    return {
+        displayArray,
+    }
 
 })();
 
+const displayControllerModule = (() => {
+
+})();
 
 const playerFactory = (name, marker) => {
     const getName  = () => name;
@@ -14,3 +32,5 @@ const playerFactory = (name, marker) => {
 
 const player1 = playerFactory('John', 'X');
 console.log(player1.getName());
+
+GameBoardModule.displayArray();
