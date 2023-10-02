bad21aa12a393478f7df8bf1fb0f9228552a570a
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-10-02 15:41:50 -0400

    Remove 2D array and conver to normal and add function to change value at index

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
 
 const boardModule = (() => {
-    let board = [];
-    const rows = 3;
-    const columns = 3;
+    let board = ['X','X','X',
+                 'O','O','O',
+                 'X','X','X'];
+    // const rows = 3;
+    // const columns = 3;
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push('');
-        }
-    }
+    // for (let i = 0; i < rows; i++) {
+    //     board[i] = [];
+    //     for (let j = 0; j < columns; j++) {
+    //         board[i].push('');
+    //     }
+    // }
 
     console.log(board);
 
     const getBoard = () => board;
-    
-    const displayArray = () => {
-        const displayVar = document.querySelector('.game-container');
-            board.forEach((element) => {
-                displayVar.innerHTML += element;
-            })
-        // const displayVar = document.querySelector('.game-container');
-        // for (let i = 0; i < board.length; i++) {
-        //     displayVar.innerHTML += board[i];
-        // }
+
+    const placeMarker = (coord, player) => {
+        board[coord] = player;
+
     }
+    
+    // const displayArray = () => {
+    //     const displayVar = document.querySelector('.game-container');
+    //         board.forEach((element) => {
+    //             displayVar.innerHTML += element;
+    //         })
+    //     // const displayVar = document.querySelector('.game-container');
+    //     // for (let i = 0; i < board.length; i++) {
+    //     //     displayVar.innerHTML += board[i];
+    //     // }
+    // }
 
     return {
-        displayArray,
+        // displayArray,
+        getBoard,
+        placeMarker,
+
     }
 
 })();
 
-const displayControllerModule = (() => {
+const gameStateModule = (() => {
+    // switch player turns
+    // tell which player to take turn
+
 
 })();
 
@@ -46,4 +59,27 @@ const playerFactory = (name, marker) => {
 const player1 = playerFactory('John', 'X');
 console.log(player1.getName());
 
-boardModule.displayArray();
+console.log(boardModule.placeMarker(1,'x'));
+console.log(boardModule.getBoard());
+
+// player1.placeMarker(1,1,'x');
+
+
+
+// function Cell() {
+//     let value = 0;
+
+//     const addToken = (player) => {
+//         value = player;
+//     }
+
+//     return {
+//         addToken,
+//         get value() { return value }
+//     };
+// }
+
+
+// const array = [Cell(), Cell(), Cell()]
+// array[0].addToken("x")
+// console.log(array[0].value)
