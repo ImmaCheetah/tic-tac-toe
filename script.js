977a7aa7c87ba217cfabfdc2b90f7957129c243d
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-10-03 13:24:13 -0400

    Move console logs into game state module

--- script.js
+++ script.js
@@ -1,51 +1,19 @@
 
 const boardModule = (() => {
-    let board = ['X','X','X',
-                 'O','O','O',
-                 'X','X','X'];
-    // const rows = 3;
-    // const columns = 3;
-
-    // for (let i = 0; i < rows; i++) {
-    //     board[i] = [];
-    //     for (let j = 0; j < columns; j++) {
-    //         board[i].push('');
-    //     }
-    // }
-
-    console.log(board);
+    let board = ['','','',
+                 '','','',
+                 '','',''];
 
     const getBoard = () => board;
 
-    const placeMarker = (coord, player) => {
-        board[coord] = player;
-
+    const placeMarker = (spot, player) => {
+        board[spot] = player;
     }
-    
-    // const displayArray = () => {
-    //     const displayVar = document.querySelector('.game-container');
-    //         board.forEach((element) => {
-    //             displayVar.innerHTML += element;
-    //         })
-    //     // const displayVar = document.querySelector('.game-container');
-    //     // for (let i = 0; i < board.length; i++) {
-    //     //     displayVar.innerHTML += board[i];
-    //     // }
-    // }
 
     return {
-        // displayArray,
         getBoard,
         placeMarker,
-
     }
-
-})();
-
-const gameStateModule = (() => {
-    // switch player turns
-    // tell which player to take turn
-
 
 })();
 
@@ -56,13 +24,26 @@ const playerFactory = (name, marker) => {
     return {getName, getMarker};
 };
 
-const player1 = playerFactory('John', 'X');
-console.log(player1.getName());
+const gameStateModule = (() => {
+    // place player markers
+    // switch player turns
+    // tell which player to take turn
+    const player1 = playerFactory('John', 'X');
+const player2 = playerFactory('Bob', 'O');
 
 console.log(boardModule.placeMarker(1,'x'));
 console.log(boardModule.getBoard());
+console.log(boardModule.placeMarker(3,'o'));
+console.log(boardModule.getBoard());
 
-// player1.placeMarker(1,1,'x');
+
+})();
+
+
+
+
+
+// console.log(player1.placeMarker(1,'x'));
 
 
 
