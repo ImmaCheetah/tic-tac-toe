6da31b40afa4e51cfdca5a5b846c6d77acbdd05c
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-10-19 11:10:22 -0400

    Commit to update file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Module for the game board to store array of board and place marker
 const gameboardModule = (() => {
-    let board = ['0','1','2',
-                 '3','4','5',
-                 '6','7','8'];
+    let board = ['','','',
+                 '','','',
+                 '','',''];
 
     const getBoard = () => board;
 
@@ -36,8 +36,8 @@ const gameControllerModule = (() => {
     let placeMarker = gameboardModule.placeMarker;
     
 
-    const playerOne = playersFactory('Bob', 'X');
-    const playerTwo = playersFactory('Tom', 'O');
+    const playerOne = playersFactory('Bob', 'O');
+    const playerTwo = playersFactory('Tom', 'X');
 
     const players = [
         {
@@ -54,13 +54,44 @@ const gameControllerModule = (() => {
     const getCurrentPlayer = () => currentPlayer;
 
     const makeTurn = () => {
+        console.log(getBoard().toString().replace(/\,/g,''));
+        // let answer = prompt('where you want it boss');
         
-        placeMarker(prompt('where'), getCurrentPlayer().marker);
-        switchPlayer();
         
-        printNewRound();
-        checkForWin();
+        // if (getBoard().toString().replace(/\,/g,'').charAt(answer) === '') {
+        //     placeMarker(answer, getCurrentPlayer().marker);
+        //     switchPlayer();
+        //     printNewRound();
+        //     checkForWin();  
+        // } else if (getBoard().toString().replace(/\,/g,'').charAt(answer) !== '') {
+        //     console.log('Cant put it there');
+        //     placeMarker(prompt('pick new spot'), getCurrentPlayer().marker);
+        // } else {
+        //     console.log('Error');
+        // }
+
+        for (let i = 0; i < 10; i++) {
+            let answer = prompt('where you want it boss');
+            if (getBoard().toString().replace(/\,/g,'1').charAt(answer) === '') {
+                placeMarker(answer, getCurrentPlayer().marker);
+                switchPlayer();
+                printNewRound();
+                checkForWin();
+                i++;  
+            } else if (getBoard().toString().replace(/\,/g,'').charAt(answer) !== '') {
+                console.log('Cant put it there');
+                return;
+                // answer = prompt('gonna have to go somewhere else m8');
+                // placeMarker(answer, getCurrentPlayer().marker);
+                // i++;
+            }
+        }
     }
+        // start loop
+        // prompt user where to place marker
+        // use answer to check if place is empty
+        // if empty, place marker, switch player, check win
+        // if not empty, prompt for new answer
 
     const switchPlayer = () => {
         if (getCurrentPlayer() === players[1]) {
@@ -71,8 +102,9 @@ const gameControllerModule = (() => {
     }
 
     const printNewRound = () => {
-        console.log(getBoard());
         console.log(`It's ${getCurrentPlayer().name}'s turn`);
+        console.log(getBoard());
+        
     }
 
     printNewRound();
@@ -120,37 +152,6 @@ const gameControllerModule = (() => {
 const game = gameControllerModule;
 
 console.log(game.makeTurn());
-console.log(game.makeTurn());
 
 
 
-
-
-// const playboard = gameboardModule;
-// console.log(gameControllerModule.getCurrentPlayer());
-// console.log(playboard.currentPlayer = 'jon');
-// console.log(playboard.currentPlayer);
-
-
-
-// const items = [
-//     { name: 'Widget', price: 19.99 },
-//     { name: 'Gizmo', price: 29.99 },
-//     { name: 'Doodad', price: 9.99 },
-//     { name: 'Contraption', price: 49.99 },
-//     { name: 'Gadget', price: 14.95 },
-//     { name: 'Trinket', price: 39.99 },
-//     { name: 'Gubbins', price: 22.50 },
-//     { name: 'Thingamajig', price: 7.99 },
-//     { name: 'Doohickey', price: 59.99 },
-//     { name: 'Gismo', price: 12.49 },
-//   ];
-
-
-// const fileteredItems = items.filter((items) => {
-//     items.price < 10
-// });
-
-// console.log(fileteredItems);
-
-// console.log(player1.placeMarker(1,'x'));
